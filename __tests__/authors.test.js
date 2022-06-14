14aa579a24c14afb4ef3e1438a0855209784ac1d
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-14 10:12:21 -0700

    some more file setup

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -3,13 +3,15 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('characters routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('example test - delete me!', () => {
     expect(1).toEqual(1);
   });
+
   afterAll(() => {
     pool.end();
   });
